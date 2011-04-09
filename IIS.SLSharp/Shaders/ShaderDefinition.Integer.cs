4e710e4e9f682a59a696c75d8ab2bbae5398221e
Wolfgang Steffens <WolfgangSteff@gmail.com>
2011-04-09 18:56:05 +0200

    added support for the following integer builtins
    genUType uaddCarry (genUType x, genUType y, out genUType carry)
    genUType usubBorrow (genUType x, genUType y, out genUType borrow)
    void umulExtended (genUType x, genUType y, out genUType msb, out genUType lsb)
    void imulExtended (genIType x, genIType y, out genIType msb, out genIType lsb)

--- IIS.SLSharp/Shaders/ShaderDefinition.Integer.cs
+++ IIS.SLSharp/Shaders/ShaderDefinition.Integer.cs
@@ -4,11 +4,153 @@ namespace IIS.SLSharp.Shaders
 {
     public abstract partial class ShaderDefinition
     {
-        // TODO: implement the following prototypes when we have out support
-        // genUType uaddCarry (genUType x, genUType y, out genUType carry)
-        // genUType usubBorrow (genUType x, genUType y, out genUType borrow)
-        // void umulExtended (genUType x, genUType y, out genUType msb, out genUType lsb)
-        // void imulExtended (genIType x, genIType y, out genIType msb, out genIType lsb)
+        #region genUType uaddCarry (genUType x, genUType y, out genUType carry)
+
+        /// <summary> Adds 32-bit unsigned integer x and y, returning the sum modulo 2**32. 
+        /// The value carry is set to 0 if the sum was less than 232, or to 1 otherwise. </summary>
+        protected static uint uaddCarry(uint x, uint y, out uint carry)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary> Adds 32-bit unsigned integer x and y, returning the sum modulo 2**32. 
+        /// The value carry is set to 0 if the sum was less than 232, or to 1 otherwise. </summary>
+        protected static uvec2 uaddCarry(uvec2 x, uvec2 y, out uvec2 carry)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary> Adds 32-bit unsigned integer x and y, returning the sum modulo 2**32. 
+        /// The value carry is set to 0 if the sum was less than 232, or to 1 otherwise. </summary>
+        protected static uvec3 uaddCarry(uvec3 x, uvec3 y, out uvec3 carry)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary> Adds 32-bit unsigned integer x and y, returning the sum modulo 2**32. 
+        /// The value carry is set to 0 if the sum was less than 232, or to 1 otherwise. </summary>
+        protected static uvec4 uaddCarry(uvec4 x, uvec4 y, out uvec4 carry)
+        {
+            throw _invalidAccess;
+        }
+
+        #endregion
+
+        #region genUType usubBorrow (genUType x, genUType y, out genUType borrow)
+
+        /// <summary> Subtracts the 32-bit unsigned integer y from x, returning the difference 
+        /// if non-negative, or 2**32 plus the difference otherwise. 
+        /// The value borrow is set to 0 if x >= y, or to 1 otherwise. </summary>
+        protected static uint usubBorrow(uint x, uint y, out uint borrow)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary> Subtracts the 32-bit unsigned integer y from x, returning the difference 
+        /// if non-negative, or 2**32 plus the difference otherwise. 
+        /// The value borrow is set to 0 if x >= y, or to 1 otherwise. </summary>
+        protected static uvec2 usubBorrow(uvec2 x, uvec2 y, out uvec2 borrow)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary> Subtracts the 32-bit unsigned integer y from x, returning the difference 
+        /// if non-negative, or 2**32 plus the difference otherwise. 
+        /// The value borrow is set to 0 if x >= y, or to 1 otherwise. </summary>
+        protected static uvec3 usubBorrow(uvec3 x, uvec3 y, out uvec3 borrow)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary> Subtracts the 32-bit unsigned integer y from x, returning the difference 
+        /// if non-negative, or 2**32 plus the difference otherwise. 
+        /// The value borrow is set to 0 if x >= y, or to 1 otherwise. </summary>
+        protected static uvec4 usubBorrow(uvec4 x, uvec4 y, out uvec4 borrow)
+        {
+            throw _invalidAccess;
+        }
+
+        #endregion
+
+        #region void umulExtended (genUType x, genUType y, out genUType msb, out genUType lsb)
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void umulExtended(uint x, uint y, out uint msb, out uint lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void umulExtended(uvec2 x, uvec2 y, out uvec2 msb, out uvec2 lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void umulExtended(uvec3 x, uvec3 y, out uvec3 msb, out uvec3 lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void umulExtended(uvec4 x, uvec4 y, out uvec4 msb, out uvec4 lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        #endregion
+
+        #region void imulExtended (genIType x, genIType y, out genIType msb, out genIType lsb)
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void imulExtended(int x, int y, out int msb, out int lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void imulExtended(ivec2 x, ivec2 y, out ivec2 msb, out ivec2 lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void imulExtended(ivec3 x, ivec3 y, out ivec3 msb, out ivec3 lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        /// <summary>
+        /// Multiplies 32-bit integers x and y, producing a 64-bit result. 
+        /// The 32 least-significant bits are returned in lsb. The 32 most-significant bits are returned in msb.
+        /// </summary>
+        protected static void imulExtended(ivec4 x, ivec4 y, out ivec4 msb, out ivec4 lsb)
+        {
+            throw _invalidAccess;
+        }
+
+        #endregion
 
         #region genIType bitfieldExtract (genIType value, int offset, int bits)
 
