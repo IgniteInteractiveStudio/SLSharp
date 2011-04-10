1a9eefaeade8fd80c36ae52bb927e71ad74ffe43
Wolfgang Steffens <WolfgangSteff@gmail.com>
2011-04-10 19:32:22 +0200

    added dmat versions of matrix builtins

--- IIS.SLSharp/Shaders/ShaderDefinition.MatrixFunctions.cs
+++ IIS.SLSharp/Shaders/ShaderDefinition.MatrixFunctions.cs
@@ -62,6 +62,64 @@ namespace IIS.SLSharp.Shaders
 
         #endregion
 
+        #region dmat matrixCompMult (dmat x, dmat y)
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat2 matrixCompMult(dmat2 x, dmat2 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat2x3 matrixCompMult(dmat2x3 x, dmat2x3 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat2x4 matrixCompMult(dmat2x4 x, dmat2x4 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat3x2 matrixCompMult(dmat3x2 x, dmat3x2 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat3 matrixCompMult(dmat3 x, dmat3 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat3x4 matrixCompMult(dmat3x4 x, dmat3x4 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat4x2 matrixCompMult(dmat4x2 x, dmat4x2 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat4x3 matrixCompMult(dmat4x3 x, dmat4x3 y) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Multiply matrix x by matrix y component-wise, i.e., result[i][j] is the scalar product of x[i][j] and y[i][j].
+        /// Note: to get linear algebraic matrix multiplication, use the multiply operator (*).
+        /// </summary>
+        public dmat4 matrixCompMult(dmat4 x, dmat4 y) { throw _invalidAccess; }
+
+        #endregion
+
         #region outerProduct
 
         /// <summary>
@@ -174,6 +232,118 @@ namespace IIS.SLSharp.Shaders
 
         #endregion
 
+        #region outerProduct (double)
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat2 outerProduct(dvec2 c, dvec2 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat3 outerProduct(dvec3 c, dvec3 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat4 outerProduct(dvec4 c, dvec4 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat2x3 outerProduct(dvec3 c, dvec2 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat3x2 outerProduct(dvec2 c, dvec3 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat2x4 outerProduct(dvec4 c, dvec2 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat4x2 outerProduct(dvec2 c, dvec4 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat3x4 outerProduct(dvec4 c, dvec3 r) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Treats the first parameter c as a column vector (matrix with one column) 
+        /// and the second parameter r as a row vector (matrix with one row) 
+        /// and does a linear algebraic matrix multiply c * r, yielding a matrix 
+        /// whose number of rows is the number of components in c and whose number 
+        /// of columns is the number of components in r.
+        /// </summary>
+        /// <param name="c">left side column vector</param>
+        /// <param name="r">right side row vector</param>
+        /// <returns></returns>
+        public dmat4x3 outerProduct(dvec3 c, dvec4 r) { throw _invalidAccess; }
+
+        #endregion
+
         #region transpose
 
         /// <summary>
@@ -232,6 +402,64 @@ namespace IIS.SLSharp.Shaders
 
         #endregion
 
+        #region transpose (double)
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat2 transpose(dmat2 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat3 transpose(dmat3 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat4 transpose(dmat4 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat2x3 transpose(dmat3x2 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat3x2 transpose(dmat2x3 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat2x4 transpose(dmat4x2 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat4x2 transpose(dmat2x4 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat3x4 transpose(dmat4x3 m) { throw _invalidAccess; }
+
+        /// <summary>
+        /// Returns a matrix that is the transpose of m. 
+        /// The input matrix m is not modified.
+        /// </summary>
+        public dmat4x3 transpose(dmat3x4 m) { throw _invalidAccess; }
+
+        #endregion
+
         #region determinant
 
         /// <summary>Returns the determinant of m. </summary>
@@ -242,6 +470,19 @@ namespace IIS.SLSharp.Shaders
 
         /// <summary>Returns the determinant of m. </summary>
         public float determinant(mat4 m) { throw _invalidAccess; }
+
+        #endregion
+
+        #region determinant (double)
+
+        /// <summary>Returns the determinant of m. </summary>
+        public float determinant(dmat2 m) { throw _invalidAccess; }
+
+        /// <summary>Returns the determinant of m. </summary>
+        public float determinant(dmat3 m) { throw _invalidAccess; }
+
+        /// <summary>Returns the determinant of m. </summary>
+        public float determinant(dmat4 m) { throw _invalidAccess; }
 
         #endregion
     }
