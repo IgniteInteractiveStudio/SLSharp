d5777c035aff86ff5b48184052b54e452e86d6f7
alexrp <xtzgzorex@gmail.com>
2011-10-16 16:58:53 +0200

    Update Copyright in SolutionInfo.cs.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,9 +4,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Ignite Interactive Studio")]
 [assembly: AssemblyProduct("SL#")]
-[assembly: AssemblyCopyright("Copyright © Ignite Interactive Studio")]
-[assembly: AssemblyTrademark("MIT (Expat) License")]
+[assembly: AssemblyCopyright("Copyright © 2011 Ignite Interactive Studio")]
+
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
+
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
